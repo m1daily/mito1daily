7e5890b80c8f51249ea70cd753ae5cc93332f22d
SoniPana <89836078+SoniPana@users.noreply.github.com>
2024-08-28 13:08:36 +0900

    ♻️Refactor: 回数調整
    - 連打回数の調整

--- src/scripts/daily.js
+++ src/scripts/daily.js
@@ -1,14 +1,13 @@
 let clickCount = 0
 let timer = null
-const timeout = 500
+const timeout = 1000
 const hogeElement = document.getElementById('daily-image')
 
-// クリック回数によって動作を分岐
 hogeElement.addEventListener('click', () => {
     clickCount += 1
     if (clickCount === 1) {
         timer = setTimeout(() => {
-            if (clickCount >= 5) {
+            if (clickCount >= 13) {
                 window.location.href = '/unknown';
             }
             timer = null
