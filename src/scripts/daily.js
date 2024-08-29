7be55624596c273b6ed144011cc0114e00268464
SoniPana <89836078+SoniPana@users.noreply.github.com>
2024-08-29 20:23:39 +0900

    ♻Refactor: Update click count threshold for redirect
    - Adjusted the click count threshold for redirecting to the unknown page in the daily.js file
    - Updated the timeout value from 1000 to 700 milliseconds

--- src/scripts/daily.js
+++ src/scripts/daily.js
@@ -1,13 +1,13 @@
 let clickCount = 0
 let timer = null
-const timeout = 1000
+const timeout = 700
 const hogeElement = document.getElementById('daily-image')
 
 hogeElement.addEventListener('click', () => {
     clickCount += 1
     if (clickCount === 1) {
         timer = setTimeout(() => {
-            if (clickCount >= 13) {
+            if (clickCount >= 7) {
                 window.location.href = '/unknown';
             }
             timer = null
