ca03e0458e34f5136ef449178d13ac53cd37ca81
SoniPana <89836078+SoniPana@users.noreply.github.com>
2024-09-03 01:57:00 +0900

    🚀Perf: 画像取得の際の名称変更
    - 例: soni→soni.jpg

--- src/scripts/images.js
+++ src/scripts/images.js
@@ -3,6 +3,7 @@ async function getImages(files) {
   const images = files.reduce((acc, file) => {
     const image = file.default;
     const fileName = image.src.split('/').pop().split('.')[0]; // ファイル名を抽出
+    const fileName2 = image.src.split('/').pop().split('?')[0]; // 拡張子を含めてファイル名を抽出
     acc[fileName] = image;
     return acc;
   }, {});
